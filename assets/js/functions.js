4c3877a25e8cffe3b97c954fe7a105e3aae39243
Chris Naus <NausDesign@gmail.com>
2016-05-13 15:57:18 -0500

    updated JS
    added smooth scrolling

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,26 @@
 $( document ).ready(function() {
 
-  // Get started!
+  // funcitions to call ----------------------
+
+  $(function() {
+  	smoothScroll(300);
+  });
+
+
+  // smoothScroll function ----------------------
+
+  function smoothScroll (duration) {
+  	$('a[href^="#"]').on('click', function(event) {
+
+  	    var target = $( $(this).attr('href') );
+
+  	    if( target.length ) {
+  	        event.preventDefault();
+  	        $('html, body').animate({
+  	            scrollTop: target.offset().top
+  	        }, duration);
+  	    }
+  	});
+  }
 
 });
